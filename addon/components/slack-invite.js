02366424db66c95ccaf9ace3226fb4b849751738
Brandon J McKay <admin@brandonjmckay.com>
2015-08-14 13:51:16 -0700

    Create the initial slack-invite component

---
+++ addon/components/slack-invite.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+import layout from '../templates/components/slack-invite';
+import ajax from 'ic-ajax';
+const { Component } = Ember;
+
+export default Component.extend({
+  layout:   layout,
+
+  ajaxError:   null,
+  ajaxOptions: null,
+  apiToken:    null,
+  apiUrl:      null,
+  email:       null,
+
+  ajaxOptions(){
+    url: 'https://' + this.get('apiUrl') + '/api/users.admin.invite',
+    form: {
+      email:      this.get('email'),
+      token:      this.get('apiToken'),
+      set_active: true
+    }
+  },
+
+  actions: {
+    request(){
+      ajax(this.ajaxOptions()).then(
+        response => {
+          this.sendAction('ajaxResponse', response);
+        },
+        error => {
+          this.sendAction('ajaxError', error);
+        }
+      );
+    }
+  }
+});
---
+++ app/components/slack-invite.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-slack-invite/components/slack-invite';
